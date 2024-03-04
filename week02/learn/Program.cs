42ccd0365616271421e43c59e1cc448fb5d1b71d
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-03-04 08:37:29 -0700

    Learn Activity Completion

--- week02/learn/Program.cs
+++ week02/learn/Program.cs
@@ -1,6 +1,14 @@
-﻿Console.WriteLine("\n======================\nSimple Queue\n======================");
+﻿Console.WriteLine("\n======================\nSimple Stack\n======================");
+SimpleStack.Run();
+
+// Uncomment and run as you get to the solution part
+Console.WriteLine("\n======================\nSimple Stack\n======================");
+SimpleStackSolution.Run();
+
+
+Console.WriteLine("\n======================\nSimple Queue\n======================");
 SimpleQueue.Run();
 
 // Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nSimple Queue\n======================");
-// SimpleQueueSolution.Run();
+Console.WriteLine("\n======================\nSimple Queue\n======================");
+SimpleQueueSolution.Run();
